42719ecdae484d8bd72f62a5bff85a2180642866
Vtraxio <55654178+Vtraxio@users.noreply.github.com>
2025-04-13 17:00:37 +0200

    Rozwiązania INF.04-02-24.01-SG / INF.04-01-24.01-SD / INF.04-01-24.06-SD konsola C#

---
+++ INF.04-24.01/INF.04-02-24.01-SG/konsola/wersja w C#/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Write("Wpisz jakiś tekst: ");
+var str = Console.ReadLine() ?? "";
+Console.WriteLine($"Liczba samogłosek: {StringFunctions.CountVowels(str)}");
+Console.WriteLine($"Tekst bez duplikatów: {StringFunctions.RemoveDuplicate(str)}");
+
+/**************************************************************
+  Klasa:	StringFunctions
+  Opis:		Reprezentuje małą bibliotekę tekstową
+  Metody:	CountVowels - zwraca liczbę zamogłosek
+			RemoveDuplicate - zwraca tekst z parametru bez duplikatów obok siebie.
+  Autor:	01234567890
+**************************************************************/
+class StringFunctions {
+	public static int CountVowels(string str) {
+		var vowels = "aąeęiouóy";
+
+		if (str is null or "")
+			return 0;
+
+		return str.ToCharArray().Count(x => vowels.Contains(char.ToLower(x)));
+	}
+
+	public static string RemoveDuplicate(string str) {
+		var toRet = string.Empty;
+		
+		if (str is null or "")
+			return string.Empty;
+
+		var last = '\0';
+		foreach (var c in str) {
+			if (c == last)
+				continue;
+
+			toRet += c;
+			last  =  c;
+		}
+
+		return toRet;
+	}
+}
